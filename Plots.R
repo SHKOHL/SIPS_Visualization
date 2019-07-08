b5bc2bb8d1f45b2673b68887e22450b538505701
skohl <skohl@ukaachen.de>
2019-07-08 16:13:43 +0200

    test

--- Plots.R
+++ Plots.R
@@ -8,7 +8,7 @@ full_trains <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidyt
 # libraries
 library(dplyr)
 library(ggplot2)
-
+library(stringr)
 
 full_trains %>% 
   ggplot(aes(x = journey_time_avg)) + 
@@ -30,6 +30,17 @@ full_trains %>%
        title = "Histogram of Average Journey Time for trains on the SNCF network")
 
 
+paris_only <- full_trains %>%  filter(str_detect(departure_station, "^P"))
+
+paris_only_2 <-   with(paris_only(aggregate(paris_only$Average_cancelation, by = list(year = year, month = month, departure_station = departure_station),mean)))
+  
+full_trains$Avg_canc_month <- full_trains$num_of_canceled_trains[full_trains$num_of_canceled_trains == 
+  month
+  year
+full_trains %>%
+  ggplot(aes(x = num_of_cancelled_trains, y = departure_station)) + 
+  geom_point() +
+  facet_wrap(~year)
 
 
 
--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 # install.R
 install.packages("ggplot2")
 install.packages("dplyr")
+install.packages("stringr")
